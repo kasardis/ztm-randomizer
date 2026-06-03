757ae079d01f1983a2e865d3ca6e738637b6f0e5
Katarzyna Szwed <kasia.szwed17@interia.pl>
2026-06-03 22:33:28 +0200

    fixes

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ fetch("./gtfs/stops.txt")
 var route_data;
 fetch("./gtfs/routes.txt")
 .then(r=>r.text())
-.then(text => route_data = text)
+.then(text => route_data = text.split('\n'))
 .then( () => {
     for(var i = 0; i < route_data.length; i++){
         route_data[i] = route_data[i].split(',');
@@ -19,13 +19,13 @@ fetch("./gtfs/routes.txt")
 });
 
 document.querySelector("#random-stop").addEventListener('click', function(e) {
-    var index = Math.random() % stop_data.length;
+    var index = Math.floor(Math.random() * stop_data.length - 1);
     document.querySelector("#result").textContent = stop_data[index][2];
     document.querySelector("#result-description").textContent = stop_data[index][3] + ', ' + stop_data[index][4];
 });
 
 document.querySelector("#random-route").addEventListener('click', function(e) {
-    var index = Math.random() % route_data.length;
+    var index = Math.floor(Math.random() * route_data.length - 1);
     document.querySelector("#result").textContent = route_data[index][2];
     document.querySelector("#result-description").textContent = route_data[index][4];
 });
