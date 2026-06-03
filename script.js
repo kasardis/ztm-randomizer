1a112af1389eef62e0b1fa7a1bfad499541d61ba
Katarzyna Szwed <kasia.szwed17@interia.pl>
2026-06-03 22:01:01 +0200

    stop generation

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
-var stop_data = "";
-
+var stop_data;
 fetch("./gtfs/stops.txt")
 .then(r=>r.text())
-.then(text => stop_data = text);
+.then(text => stop_data = text.split('\n'));
+
+for(var i = 0; i < stop_data.length; i++){
+    stop_data[i] = stop_data.split(',');
+}
+
+var route_data = "";
+fetch("./gtfs/routes.txt")
+.then(r=>r.text())
+.then(text => route_data = text);
 
 document.querySelector("#random-stop").addEventListener('click', function(e) {
-    document.querySelector("#result").textContent = "hello world";
-    document.querySelector("#result-description").textContent = "hello world 2";
+    var index = Math.random() % stop_data.length;
+    document.querySelector("#result").textContent = stop_data[index][2];
+    document.querySelector("#result-description").textContent = stop_data[index][3] + ', ' + stop_data[index][4];
 });
 
 document.querySelector("#random-route").addEventListener('click', function(e) {
